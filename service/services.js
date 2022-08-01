45d05f911860282c52e0ad167c0247bdd96a337b
Federico <federeinoso01@gmail.com>
2022-08-01 05:38:10 -0300

    web responsive

--- service/services.js
+++ service/services.js
@@ -1,5 +1,5 @@
 const newItem = (categoria) =>
-  fetch(`https://localhost:3000/${categoria}`).then((respuesta) =>
+  fetch(`https://server-alura.herokuapp.com/${categoria}`).then((respuesta) =>
     respuesta.json()
   );
 
